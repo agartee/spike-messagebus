263f87b8dd7f6260353f34dc0d0aa28ab6ebfcab
Adam Gartee <agartee@gmail.com>
2025-07-25 16:14:42 -0400

    added migration

---
+++ src/Spike.SqlServer/Migrations/20250725201227_Initial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spike.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Spike");
+
+            migrationBuilder.CreateTable(
+                name: "MessageOutbox",
+                schema: "Spike",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TypeName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommitSequence = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    SendAttemptCount = table.Column<int>(type: "int", nullable: false),
+                    LastDequeuedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageOutbox", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                schema: "Spike",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageOutbox",
+                schema: "Spike");
+
+            migrationBuilder.DropTable(
+                name: "People",
+                schema: "Spike");
+        }
+    }
+}
